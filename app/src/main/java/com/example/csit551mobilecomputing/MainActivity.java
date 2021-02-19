183873060ff04a6060989210b48cb0a0bcaa8b8a
shayxt <39503425+shayxt@users.noreply.github.com>
2021-02-19 08:49:51 -0500

    First Homework Submission Seyi Ojewale Mobile Computing

--- app/src/main/java/com/example/csit551mobilecomputing/MainActivity.java
+++ app/src/main/java/com/example/csit551mobilecomputing/MainActivity.java
@@ -1,7 +1,7 @@
 package com.example.csit551mobilecomputing;
-
+import android.content.Context;
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.widget.Toast;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +10,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Context context = getApplicationContext();
+        CharSequence text = "Initialising Hello World...";
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
     }
 }
